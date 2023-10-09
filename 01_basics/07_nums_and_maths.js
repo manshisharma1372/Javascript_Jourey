fd56882d9e48942e008d7ccefac2853bb3dd4b3d
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-10-09 05:56:46 +0000

    nums and math

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,9 @@
+
+const score=400 //Number
+console.log(score); //400
+
+const balance=new Number(100)
+console.log(balance); //Number:100
+
+console.log(balance.toString());
+console.log(typeof balance);
