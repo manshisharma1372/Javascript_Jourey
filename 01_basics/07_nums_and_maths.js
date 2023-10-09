6cbc54ac59f40c925fe42ad818575db1da45dd8a
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-10-09 13:33:03 +0000

    nums and math sorted

--- 01_basics/07_nums_and_maths.js
+++ 01_basics/07_nums_and_maths.js
@@ -13,3 +13,28 @@ console.log(balance.toFixed(2)); //100.00
 const otherNumber=23.8966
 
 console.log(otherNumber.toPrecision(3)); //23.9
+
+const hund=1000000
+console.log(hund.toLocaleString()); // 1,000,000 have commas
+
+console.log(hund.toLocaleString('en-IN')); //10,00,000 acc to indian system comma will be provided
+
+//************************MATHS*************************** */
+
+console.log(Math); //object
+
+console.log(Math.abs(-1)); //1
+console.log(Math.round(4.3)); //4 regular roundoff\
+console.log(Math.ceil(4.2)); //top val 5
+console.log(Math.floor(4.9)); // 4
+
+console.log(Math.min(4,3,5,9)); //3
+console.log(Math.max(8,9,5,6,4));//9
+
+console.log(Math.random()); //0 to 1
+console.log((Math.random() *10 )+1); //to avoid mult with 0
+
+const min=10
+const max=20
++ min
+console.log(Math.floor(Math.random() * (max-min +1) ) +min );
