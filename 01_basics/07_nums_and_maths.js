abb68af50abcf4038ae16eaac4a8f469be541125
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-10-09 06:07:10 +0000

    nums and math

--- 01_basics/07_nums_and_maths.js
+++ 01_basics/07_nums_and_maths.js
@@ -5,5 +5,11 @@ console.log(score); //400
 const balance=new Number(100)
 console.log(balance); //Number:100
 
-console.log(balance.toString());
-console.log(typeof balance);
+console.log(balance.toString().length); //3
+console.log(typeof balance); //object
+
+console.log(balance.toFixed(2)); //100.00
+
+const otherNumber=23.8966
+
+console.log(otherNumber.toPrecision(3)); //23.9
