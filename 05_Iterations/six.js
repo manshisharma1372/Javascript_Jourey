90b4104f42ba333dc413a34ba7c83ecafdf8abe8
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-10-24 10:27:50 +0000

    six

---
+++ 05_Iterations/six.js
@@ -0,0 +1,15 @@
+const coding=["js","python","java","c++"]
+
+const val=coding.forEach((item) => {
+    console.log(item); 
+    /*
+js
+python
+java
+c++*/
+
+});
+
+console.log(val); //undefined
+// for each don't return val
+
